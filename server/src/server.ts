25222393d0837bb0b96b1fd209509a379556342d
Samuel Gomes <samuel_gomes26@hotmail.com>
2025-01-08 14:06:53 -0300

    ✨ (server.ts): adiciona configuração de CORS para permitir origens específicas

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,12 @@ import routes from "./routes";
 
 const server = express();
 
-server.use(cors());
+server.use(
+  cors({
+    origin: ["http://wiserooms.com.br", "http://localhost:3000"],
+    credentials: true,
+  })
+);
 server.use(express.json());
 
 server.get("/", (req, res) => {
